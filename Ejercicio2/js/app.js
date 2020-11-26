be6175b3253851e9439948650b5a84cbd4e61ae0
Light-lig <lightcaceres@gmail.com>
2020-11-26 14:05:50 -0600

    animaciones

--- Ejercicio2/js/app.js
+++ Ejercicio2/js/app.js
@@ -1,8 +1,11 @@
 window.onload = () => {
-swal("Bienvenido!!!!", "Espero disfrute el contenido.");
+//swal("Bienvenido!!!!", "Espero disfrute el contenido.");
 var formulario  = document.getElementById('login');
 formulario.addEventListener('submit', (e) =>{
   swal("Bienvenido!!!!", "Se inicio sesion.");
   e.preventDefault();
 });
+gsap.from("#play", {duration: 1, y: 300, x:300, x:300, y:300, opacity: 0, scale: 0.5});
+gsap.from("#titulo", {duration: 1, y: -300, opacity: 0, scale: 0.5});
+gsap.from("#precio", {duration: 3, y: 300, x:-300, opacity: 0, scale: 0.5, ease: "bounce"});
 }
