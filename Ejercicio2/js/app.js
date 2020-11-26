db5547359016a3af0039da693db6dea4e49fdb34
Light-lig <lightcaceres@gmail.com>
2020-11-26 17:03:58 -0600

    cambios

--- Ejercicio2/js/app.js
+++ Ejercicio2/js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 gsap.from("#play", {duration: 1, y: 300, x:300, x:300, y:300, opacity: 0, scale: 0.5});
 gsap.from("#titulo", {duration: 1, y: -300, opacity: 0, scale: 0.5});
 gsap.from("#precio", {duration: 3, y: 300, x:-300, opacity: 0, scale: 0.5, ease: "bounce"});
+
   const settings = {
   	"async": true,
   	"crossDomain": true,
