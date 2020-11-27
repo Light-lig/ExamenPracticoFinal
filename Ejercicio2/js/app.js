7012d9e951006b97f08712fd6a457cfe5bf9d36d
Light-lig <lightcaceres@gmail.com>
2020-11-26 20:20:23 -0600

    comentarios api

--- Ejercicio2/js/app.js
+++ Ejercicio2/js/app.js
@@ -1,8 +1,4 @@
 $(document).ready(function() {
-gsap.from("#play", {duration: 1, y: 300, x:300, x:300, y:300, opacity: 0, scale: 0.5});
-gsap.from("#titulo", {duration: 1, y: -300, opacity: 0, scale: 0.5});
-gsap.from("#precio", {duration: 3, y: 300, x:-300, opacity: 0, scale: 0.5, ease: "bounce"});
-
   const settings = {
   	"async": true,
   	"crossDomain": true,
@@ -14,9 +10,18 @@ gsap.from("#precio", {duration: 3, y: 300, x:-300, opacity: 0, scale: 0.5, ease:
   		"x-rapidapi-host": "matchilling-chuck-norris-jokes-v1.p.rapidapi.com"
   	}
   };
+  var k = 1;
+    for (var i = 0; i < 4; i++) {
+      $.ajax(settings).done(function (response) {
+        $("#mensaje"+k).append("<p>"+response.value+"</p>");
+          k++
+      });
+    }
 
-  $.ajax(settings).done(function (response) {
-  	console.log(response);
-  });
+
+
+  gsap.from("#play", {duration: 1, y: 300, x:300, x:300, y:300, opacity: 0, scale: 0.5});
+  gsap.from("#titulo", {duration: 1, y: -300, opacity: 0, scale: 0.5});
+  gsap.from("#precio", {duration: 3, y: 300, x:-300, opacity: 0, scale: 0.5, ease: "bounce"});
 
 });
